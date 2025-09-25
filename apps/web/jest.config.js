f68f35a55f39985ae3620f7faf97e86a0cb752e4
AlBe-lang <albert08120131@gmail.com>
2025-09-25 16:00:07 +0900

    Update jest.config.js

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,2 +1,16 @@
-﻿// PASTE: apps/web/jest.config.js
+const nextJest = require('next/jest');
 
+const createJestConfig = nextJest({
+  dir: './', // Next.js 앱 경로
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
